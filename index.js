899f45bad90442b67bf5b8151c9625c42a768fd7
suryakantamangaraj <surya.socialnetworking@gmail.com>
2020-09-03 18:05:53 +0530

    upadte source for github action

---
+++ index.js
@@ -0,0 +1,51 @@
+// index.js
+require('dotenv').config();
+const Mustache = require('mustache');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const puppeteerService = require('./services/puppeteer.service');
+
+const MUSTACHE_MAIN_DIR = './main.mustache';/**
+  * DATA is the object that contains all
+  * the data to be provided to Mustache
+  * Notice the "name" and "date" property.
+*/
+
+let DATA = {
+  name: 'Surya',
+  date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Asia/Kolkata',
+  }),
+};/**
+  * A - We open 'main.mustache'
+  * B - We ask Mustache to render our file with the data
+  * C - We create a README.md file with the generated output
+  */
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+
+async function action() {
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+
+  /**
+   * Fermeture de la boutique 👋
+   */
+  await puppeteerService.close();
+}
+
+action();
---
+++ services/puppeteer.service.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+class PuppeteerService {
+  browser;
+  page;
+
+  async init() {
+    this.browser = await puppeteer.launch({
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-infobars',
+        '--window-position=0,0',
+        '--ignore-certifcate-errors',
+        '--ignore-certifcate-errors-spki-list',
+        '--incognito',
+        '--proxy-server=http=194.67.37.90:3128',
+        // '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
+      ],
+      // headless: false,
+    });
+  }
+}
+
+const puppeteerService = new PuppeteerService();
+
+module.exports = puppeteerService;
